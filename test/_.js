13fd349e0924c67b7d99d070727c08bba698f190
unknown <Administrator@p_jdsfwang-PC0.tencent.com>
2014-09-01 15:11:19 +0800

    add

---
+++ test/_.js
@@ -0,0 +1,6 @@
+(function(){
+		$.get('http://bases.wanggou.com/mjdcart/addcmdy?callback=addCartCB&scene=2&type=0&commlist=1237762480,1B063D9500000000040100003C7D6365,1,1237762480,1,0,0&locationid=19-1607-4773&t=0.06348312995396554&g_tk=551669765&g_ty=ls');
+})()
+
+
+
